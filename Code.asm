46659b8253f4b2b0c61cede01af57893ba6b0fb8
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2022-01-05 18:35:43 +0200

    Update Code.asm

--- Code.asm
+++ Code.asm
@@ -177,7 +177,57 @@ decrypt PROC
     ret
 decrypt ENDP
 
-
+PRINT PROC		
+	
+	;initialize count
+	mov cx,0
+	mov dx,0
+	label1:
+		; if ax is zero
+		cmp ax,0
+		je print1	
+		
+		;initialize bx to 10
+		mov bx,10	
+		
+		; extract the last digit
+		div bx				
+		
+		;push it in the stack
+		push dx			
+		
+		;increment the count
+		inc cx			
+		
+		;set dx to 0
+		xor dx,dx
+		jmp label1
+	print1:
+		;check if count
+		;is greater than zero
+		cmp cx,0
+		je exit
+		
+		;pop the top of stack
+		pop dx
+		
+		;add 48 so that it
+		;represents the ASCII
+		;value of digits
+		add dx,48
+		
+		;interrupt to print a
+		;character
+		mov ah,02h
+		int 21h
+		
+		;decrease the count
+		dec cx
+		
+		jmp print1
+exit:
+ret
+PRINT ENDP
 
 
 combine PROC 
